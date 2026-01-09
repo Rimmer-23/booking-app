1d1f013763c6a95d8bb085f43470bdb648036255
Rimmer-23 <rimmerfaceit2@gmail.com>
2026-01-09 14:52:48 +0100

    feat: add user registration with validation and password hashing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import type { Request, Response } from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users.ts";
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
@@ -13,9 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true})); // help parse url, 
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response)=>{
-    res.json({message:"hello from express endpoint"});
-});
+app.use("/api/users", userRoutes)
 
 app.listen(7000, ()=>{
     console.log("Server is running on port 7000");
